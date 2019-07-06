1b29164e9f382b3ce6981db4ad86cb7e8cfbbeaa
Dell_MVMS <companymvms@gmail.com>
2019-07-06 14:26:29 +0200

    add: flexbox for video align in table

--- js/js.js
+++ js/js.js
@@ -101,7 +101,8 @@ class UI {
     // Insert columns
     row.innerHTML = `
       <td>${id}</td>
-      <td><video width="320" height="240" controls><source src="./videos/${video.videoName}" type="video/mp4"></video></td>
+      <!-- <td><video width="320" height="240" controls><source src="./videos/${video.videoName}" type="video/mp4"></video></td> -->
+      <td class="videoFlex"><video width="160" height="auto" controls><source src="./videos/${video.videoName}" type="video/mp4"></video></td>
       <td>${video.projectName}</td>
       <td>${video.videoTitle}</td>
       <td>${video.yourName}</td>
@@ -342,6 +343,33 @@ document.querySelector(".openSelectVideoFile").addEventListener("click", functio
      document.querySelector(".container").classList.toggle("openClose");
    }
  });
+
+
+document.querySelector(".runBat").addEventListener("click", function() {
+  // var wshShell = new ActiveXObject("WScript.Shell");
+  // wshShell.Run("C:\\Git_Repository\\Video-Archive\\bat\\run.bat");
+
+  // var oShell = WScript.CreateObject("WScript.Shell");
+  // console.log(oShell);
+  // oShell.Exec("start D:\dir\user.bat");
+
+  // <a href="#file.bat">Batch File</a>
+
+
+  // let dataUri = 'data:./storage/json;charset=utf-8,'+ encodeURIComponent(fileJSON);
+  let dataUri = '#/bat/run.bat';
+
+  // let exportFileDefaultName = 'user.bat';
+
+  let linkElement = document.createElement('a');
+  linkElement.setAttribute('href', dataUri);
+  // linkElement.setAttribute('download', exportFileDefaultName);
+  linkElement.click();
+  linkElement.remove();
+
+  console.log("clicked!");
+
+})
 
 /* DOM Load Event: Initialization!
  * It's a very important step. Here the localStorage will be retrieve and the table
